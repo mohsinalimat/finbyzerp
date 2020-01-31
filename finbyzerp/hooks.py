5a4da7c9f1339fc2159c632ff2011b324d60141b
finbyz <info@finbyz.com>
2020-01-31 06:53:19 +0000

    Updated to version-12

--- finbyzerp/hooks.py
+++ finbyzerp/hooks.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from . import __version__ as app_version
 
 app_name = "finbyzerp"
-app_title = "Finbyzerp"
+app_title = "FinbyzErp"
 app_publisher = "Finbyz Tech Pvt Ltd"
 app_description = "FinByz ERP"
 app_icon = "octicon octicon-diff-ignored"
