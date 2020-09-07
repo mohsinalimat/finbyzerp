920b39b5543133dc4e2f0897371fed2ea218a91b
finbyz <info@finbyz.com>
2020-09-07 05:55:02 +0000

    stock diff report

---
+++ finbyzerp/finbyzerp/report/stock_gl_&_sle_difference/stock_gl_&_sle_difference.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2016, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Stock GL & SLE Difference"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"width": "80",
+			"options": "Company",
+			"default": frappe.defaults.get_default("company")
+		},
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 1,
+			"default": frappe.datetime.get_today()
+		}		
+	]
+};
--- finbyzerp/public/js/doctype_js/purchase_order.js
+++ finbyzerp/public/js/doctype_js/purchase_order.js
@@ -5,13 +5,16 @@ frappe.ui.form.on('Purchase Order', {
 	onload: (frm) => {
 		frm.trigger('naming_series');
 	},
+	transaction_date: function (frm){
+		frm.trigger('naming_series');
+	},
 	naming_series: function (frm) {
 		if (frm.doc.__islocal && frm.doc.company && !frm.doc.amended_from) {
 			frappe.call({
 				method: "finbyzerp.api.check_counter_series",
 				args: {
 					'name': frm.doc.naming_series,
-					'date': frm.doc.posting_date,
+					'date': frm.doc.transaction_date,
 					'company_series': frm.doc.company_series || null,
 				},
 				callback: function (e) {
