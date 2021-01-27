d08d651ccacae02a7e219e15a863c1fb3c7e99cf
Ubuntu <ubuntu@ip-172-31-19-173.ap-south-1.compute.internal>
2021-01-27 09:45:24 +0000

    Manifest file addes

--- finbyzerp/finbyzerp/page/print_format_editor/print_format_editor.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['print-format-editor'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Print Format Editor',
-		single_column: true
-	});
-}
