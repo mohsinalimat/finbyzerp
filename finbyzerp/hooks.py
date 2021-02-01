46d63f07108720b6fa16e1fdf814e9a2d9d3e822
finbyz <info@finbyz.com>
2021-02-01 13:47:04 +0530

    BOM Stock Calculated Report Override

--- finbyzerp/hooks.py
+++ finbyzerp/hooks.py
@@ -83,3 +83,8 @@ scheduler_events = {
 		"finbyzerp.api.daily_transaction_summary_mail"
 	]
 }
+
+# BOM Stock Calculated Report Override:
+from finbyzerp.finbyzerp.report.bom_stock_calculated import execute as bsc_execute
+from erpnext.manufacturing.report.bom_stock_calculated import bom_stock_calculated
+bom_stock_calculated.execute = bsc_execute
