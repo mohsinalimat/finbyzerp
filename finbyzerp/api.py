ff3586f97a4a339ccc00a271262c83158da0a904
finbyz <info@finbyz.com>
2021-02-04 06:48:43 +0000

    Whatsapp Code Comment

--- finbyzerp/api.py
+++ finbyzerp/api.py
@@ -15,8 +15,8 @@ import os
 import sys
 import time
 
-from WebWhatsappWrapper.webwhatsapi import WhatsAPIDriver
-from WebWhatsappWrapper.webwhatsapi.objects.message import Message
+# from WebWhatsappWrapper.webwhatsapi import WhatsAPIDriver
+# from WebWhatsappWrapper.webwhatsapi.objects.message import Message
 
 def before_insert(self, method):
 	opening_naming_series(self)
