3d678533944ef693984741472bb067660e27ec6a
finbyz <info@finbyz.com>
2021-04-08 15:03:31 +0530

    Removed console print message

--- finbyzerp/public/js/doctype_js/sales_invoice.js
+++ finbyzerp/public/js/doctype_js/sales_invoice.js
@@ -12,7 +12,6 @@ frappe.ui.form.on('Sales Invoice', {
 	naming_series: function (frm) {
 		if (frappe.meta.get_docfield("Sales Invoice", "series_value", frm.doc.name)){
 			if (frm.doc.__islocal && frm.doc.company && !frm.doc.amended_from) {
-				console.log('test')
 				frappe.call({
 					method: "finbyzerp.api.check_counter_series",
 					args: {
--- finbyzerp/public/js/doctype_js/sales_order.js
+++ finbyzerp/public/js/doctype_js/sales_order.js
@@ -12,7 +12,6 @@ frappe.ui.form.on('Sales Order', {
 	naming_series: function (frm) {
 		if (frappe.meta.get_docfield("Sales Order", "series_value", frm.doc.name)){
 			if (frm.doc.__islocal && frm.doc.company && !frm.doc.amended_from) {
-				console.log('test')
 				frappe.call({
 					method: "finbyzerp.api.check_counter_series",
 					args: {
