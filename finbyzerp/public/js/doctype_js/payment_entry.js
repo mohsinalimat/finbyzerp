abd36923e075c4fec27dae54223feadf5bd7cfc1
finbyz <info@finbyz.com>
2021-05-26 10:33:14 +0530

    add address in payment entry

--- finbyzerp/public/js/doctype_js/payment_entry.js
+++ finbyzerp/public/js/doctype_js/payment_entry.js
@@ -8,7 +8,14 @@ frappe.ui.form.on('Payment Entry', {
 		if (frm.doc.__islocal){
 		frm.trigger('naming_series');
 		}
+		cur_frm.set_query("address", function (doc) {
+			return {
+				query: "frappe.contacts.doctype.address.address.address_query",
+				filters: { link_doctype: "Customer", link_name: doc.party }
+			};
+		})
 	},
+
 	naming_series: function (frm) {
 		if (frappe.meta.get_docfield("Payment Entry", "series_value", frm.doc.name)){
 			if (frm.doc.__islocal && frm.doc.company && !frm.doc.amended_from) {
@@ -34,4 +41,6 @@ frappe.ui.form.on('Payment Entry', {
 	posting_date: function (frm) {
 		frm.trigger('naming_series');
 	},
+	
 });
+
