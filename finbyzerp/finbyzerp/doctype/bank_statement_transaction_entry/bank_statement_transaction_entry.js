14d7dd856d00cb8aa57890ce84837c9a6c9139f0
finbyz <info@finbyz.com>
2021-08-26 10:24:01 +0530

    ceramic icon added and bank statement transaction entry doctypes added

---
+++ finbyzerp/finbyzerp/doctype/bank_statement_transaction_entry/bank_statement_transaction_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Finbyz Tech Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bank Statement Transaction Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ finbyzerp/finbyzerp/doctype/bank_statement_transaction_entry/test_bank_statement_transaction_entry.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Finbyz Tech Pvt Ltd and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestBankStatementTransactionEntry(unittest.TestCase):
+	pass
